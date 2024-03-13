5a6ee7e91147cbafb52d3f610e2c4b09117684f9
inno-apfel <mawei@ucsd.edu>
2024-03-13 02:28:36 -0700

    turned on trailing slashes

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/algorithmic-business-forcasting/',
 
+  trailingSlash:true,
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'dsc-capstone-b10-3', // Usually your GitHub org/user name.
