bc485a1b3fb526fe6c477d815997da2457b92e32
inno-apfel <mawei@ucsd.edu>
2024-03-13 00:28:28 -0700

    fix baseurl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,12 +11,12 @@ const config: Config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/algorithmic-business-forecasting/',
+  baseUrl: '/algorithmic-business-forcasting/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'dsc-capstone-b10-3', // Usually your GitHub org/user name.
-  projectName: 'algorithmic-business-forecasting', // Usually your repo name.
+  projectName: 'algorithmic-business-forcasting', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
