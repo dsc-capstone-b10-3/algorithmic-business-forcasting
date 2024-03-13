e7df7d1851b91a4d254cf41cf023cbea8638d1c1
inno-apfel <mawei@ucsd.edu>
2024-03-13 03:22:03 -0700

    adjust header and footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'Business Growth Forecasting',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -37,6 +37,7 @@ const config: Config = {
       'classic',
       {
         docs: {
+          // routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -61,7 +62,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Business Growth Forecasting',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
@@ -73,9 +74,14 @@ const config: Config = {
           position: 'left',
           label: 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        {to: 'how-to-run', label: 'How to Use Our Tool', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/inno-apfel/regional-business-growth-forecasting/blob/main/report.pdf',
+          label: 'Technical Paper',
+          position: 'right',
+        },
+        {
+          href: 'https://github.com/inno-apfel/regional-business-growth-forecasting',
           label: 'GitHub',
           position: 'right',
         },
@@ -85,46 +91,46 @@ const config: Config = {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: 'Authors',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: 'Maximilian Wei',
+              href: 'https://github.com/inno-apfel',
+            },
+            {
+              label: 'Mariana Montoya',
+              href: 'https://github.com/marianamontoya',
+            },
+            {
+              label: 'Michael Lue',
+              href: 'https://github.com/mlue02',
             },
           ],
         },
         {
-          title: 'Community',
+          title: 'Credits',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: 'Domain Mentorship by Naomi Young (SANDAG)',
+              href: 'https://www.linkedin.com/in/naomi-young-34a47443',
             },
           ],
         },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
+        // {
+        //   title: 'More',
+        //   items: [
+        //     {
+        //       label: 'Blog',
+        //       to: '/blog',
+        //     },
+        //     {
+        //       label: 'GitHub',
+        //       href: 'https://github.com/facebook/docusaurus',
+        //     },
+        //   ],
+        // },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
     prism: {
       theme: prismThemes.github,
